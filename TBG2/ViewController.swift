9faf0101a1a7f2aa910b344c447d44240259f429
“KrisReid” <krisreid_06@hotmail.com>
2019-08-29 16:19:40 +0100

    Initial Commit

---
+++ TBG2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TBG2
+//
+//  Created by Kris Reid on 29/08/2019.
+//  Copyright © 2019 Kris Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
