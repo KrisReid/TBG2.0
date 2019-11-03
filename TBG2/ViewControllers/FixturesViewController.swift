20bd3068c0604479ec73c7a4fd81f0d625b75a83
“KrisReid” <krisreid_06@hotmail.com>
2019-11-03 18:30:39 +0000

    tab bar fixes

---
+++ TBG2/ViewControllers/FixturesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FixturesViewController.swift
+//  TBG2
+//
+//  Created by Kris Reid on 03/11/2019.
+//  Copyright © 2019 Kris Reid. All rights reserved.
+//
+
+import UIKit
+
+class FixturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
