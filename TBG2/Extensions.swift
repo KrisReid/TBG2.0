9cf8d5d265c786aa21011df7cb79b8471d01ae99
“KrisReid” <krisreid_06@hotmail.com>
2020-01-01 16:02:02 +0000

    UI Fixes

--- TBG2/Extensions.swift
+++ TBG2/Extensions.swift
@@ -12,6 +12,7 @@ import UIKit
 class Colours {
     var primaryBlue = UIColor( red: 98/255, green: 190/255, blue:204/255, alpha: 1.0 )
     var secondaryBlue = UIColor( red: 67/255, green: 131/255, blue:140/255, alpha: 1.0 )
+    var tertiaryBlue = UIColor( red: 37/255, green: 71/255, blue:77/255, alpha: 1.0 )
     var primaryGrey = UIColor( red: 120/255, green: 120/255, blue:120/255, alpha: 1.0 )
 }
 
