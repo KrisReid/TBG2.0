c27bb98d98d5accac02442be069c95700ccc5aa5
“KrisReid” <krisreid_06@hotmail.com>
2020-02-26 15:30:28 +0000

    Settings Starter point

--- TBG2/ViewControllers/Settings/SettingsViewController.swift
+++ TBG2/ViewControllers/Settings/SettingsViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableview.estimatedRowHeight = CGFloat(70.0)
+        tableview.rowHeight = UITableView.automaticDimension
+        tableview.register(UINib(nibName: "SettingsTableViewCell", bundle: nil), forCellReuseIdentifier: "SettingsTableViewCell")
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func btnLogoutTapped(_ sender: Any) {
@@ -23,5 +28,17 @@ class SettingsViewController: UIViewController {
     }
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 5
+       }
+       
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell") as! SettingsTableViewCell
+        
+        cell.lblTitle.text = "Hello"
+        cell.lblAnswer.text = "World"
+        
+        return cell
+    }
 
 }
